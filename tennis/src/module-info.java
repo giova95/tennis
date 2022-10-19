57d0877f8367a08a56fc52eeec57da56a990ae7f
giova95 <giovanni.angeli1@stud.unifi.it>
2022-10-19 16:34:51 +0200

    add databaseDao class, create sql and middleware

--- tennis/src/module-info.java
+++ tennis/src/module-info.java
@@ -6,4 +6,5 @@
  *
  */
 module tennis {
+	requires java.sql;
 }
--- tennis/src/tennis/databaseDAO.java
+++ tennis/src/tennis/databaseDAO.java
@@ -1,5 +1,69 @@
 package tennis;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * AbstractDAO for API CRUD
+ */
 public class databaseDAO {
-	//ciao porcodio
+	private String url = "jdbc:mysql://localhost:8080/tennisApp?useSSL=false";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "";
+
+	// select all tables
+	private static final String SELECT_ALL_USERS = "select * from utente";
+	private static final String SELECT_ALL_FIELDS = "select * from campo";
+	private static final String SELECT_ALL_RESERV = "select * from prenotazione";
+	private static final String SELECT_ALL_INSTRU = "select * from istruttore";
+
+	// insert in all tables
+	private static final String INSERT_USERS_SQL = "INSERT INTO utente"
+			+ "  (nome, cognome, eta, sesso, email, telefono, password) VALUES " + " (?, ?, ?, ?. ?, ?, ?);";
+	private static final String INSERT_FIELDS_SQL = "INSERT INTO campo"
+			+ "  (tipo, prezzo, valutazione, coperto) VALUES " + " (?, ?, ?, ?);";
+	private static final String INSERT_INSTRU_SQL = "INSERT INTO istruttore"
+			+ "  (nome, cognome, eta, sesso, email, telefono, password," + " esperienza, oreLezione, pagaOraria) "
+			+ "VALUES " + " (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+	private static final String INSERT_RESERV_SQL = "INSERT INTO prenotazione"
+			+ "  (dataOra, durata, prezzo, partecipanti, campo, istruttore," + " tipo) VALUES "
+			+ " (?, ?, ?, ?. ?, ?, ?);";
+
+	// delete from all tables
+	private static final String DELETE_USERS_SQL = "delete from utente where id = ?;";
+	private static final String DELETE_FIELDS_SQL = "delete from campo where id = ?;";
+	private static final String DELETE_RESERV_SQL = "delete from prenotazione where id = ?;";
+	private static final String DELETE_ISTRUTTORE_SQL = "delete from istruttore where id = ?;";
+
+	// update all tables
+	private static final String UPDATE_USERS_SQL = "update utente set sesso = ?, email = ?, telefono = ?, password = ? where id = ?;";
+	private static final String UPDATE_FIELDS_SQL = "update users set prezzo = ?, valutazione = ?, coperto = ? where id = ?;";
+	private static final String UPDATE_REVERS_SQL = "update users set sesso = ?, email = ?, telefono = ?, password = ?, esperienza = ?"
+			+ " oreLezione = ?, pagaOraria = ? where id = ?;";
+	private static final String UPDATE_INSTRU_SQL = "update users set dataOra = ?, durata = ?, prezzo = ?  where id = ?;";
+
+	public databaseDAO() {
+	}
+
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+
+	}
+	
+	
+
 }
