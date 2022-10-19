05bd8cff70343b53b89a3e0dc62475c34b4b0735
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2022-10-19 16:56:45 +0200

    class utente added

---
+++ tennis/src/tennis/istruttore.java
@@ -0,0 +1,25 @@
+package tennis;
+
+public class istruttore {
+	
+	private int id;
+	private String nome;
+	private String cognome;
+	private int eta;
+	private char sesso;
+	private String email;
+	private String numero;
+	private String password;
+	private short esperienza;
+	private int oreLezione;
+	private float pagaOraria;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	}
---
+++ tennis/src/tennis/utente.java
@@ -0,0 +1,46 @@
+package tennis;
+
+public class utente {
+
+	private int id;
+	private String nome;
+	private String cognome;
+	private int eta;
+	private char sesso;
+	private String email;
+	private String numero;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public char getSesso() {
+		return sesso;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+}
