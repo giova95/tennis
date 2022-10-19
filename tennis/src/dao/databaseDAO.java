a16aeee843f1fc5f7eeeb717d09607fb7370a741
giova95 <giovanni.angeli1@stud.unifi.it>
2022-10-19 17:59:26 +0200

    add insert campo API

--- tennis/src/dao/databaseDAO.java
+++ tennis/src/dao/databaseDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tennis.utente;
+import tennis.campo;
 import tennis.istruttore;
 
 /**
@@ -35,21 +36,20 @@ public class databaseDAO {
 			+ "  (nome, cognome, eta, sesso, email, telefono, password," + " esperienza, oreLezione, pagaOraria) "
 			+ "VALUES " + " (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 	private static final String INSERT_RESERV_SQL = "INSERT INTO prenotazione"
-			+ "  (dataOra, durata, prezzo, partecipanti, campo, istruttore," + " tipo) VALUES "
-			+ " (?, ?, ?, ?. ?, ?, ?);";
+			+ "  (dataOra, durata, prezzo, partecipanti, campo, istruttore," + " tipo) VALUES " + " (?, ?, ?, ?. ?, ?, ?);";
 
 	// delete from all tables SQL
 	private static final String DELETE_USERS_SQL = "delete from utente where id = ?;";
 	private static final String DELETE_FIELDS_SQL = "delete from campo where id = ?;";
 	private static final String DELETE_RESERV_SQL = "delete from prenotazione where id = ?;";
-	private static final String DELETE_ISTRUTTORE_SQL = "delete from istruttore where id = ?;";
+	private static final String DELETE_ISTRU_SQL = "delete from istruttore where id = ?;";
 
 	// update all tables SQL
 	private static final String UPDATE_USERS_SQL = "update utente set sesso = ?, email = ?, telefono = ?, password = ? where id = ?;";
 	private static final String UPDATE_FIELDS_SQL = "update users set prezzo = ?, valutazione = ?, coperto = ? where id = ?;";
-	private static final String UPDATE_REVERS_SQL = "update users set sesso = ?, email = ?, telefono = ?, password = ?, esperienza = ?"
+	private static final String UPDATE_REVERS_SQL = "update users set  dataOra = ?, durata = ?, prezzo = ? where id = ?;";
+	private static final String UPDATE_INSTRU_SQL = "update users set sesso = ?, email = ?, telefono = ?, password = ?, esperienza = ?"
 			+ " oreLezione = ?, pagaOraria = ? where id = ?;";
-	private static final String UPDATE_INSTRU_SQL = "update users set dataOra = ?, durata = ?, prezzo = ?  where id = ?;";
 
 	public databaseDAO() {}
 
@@ -74,8 +74,8 @@ public class databaseDAO {
         try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
             preparedStatement.setString(1, user.getNome());
             preparedStatement.setString(2, user.getCognome());
-            preparedStatement.setLong(3, user.getEta());
-            preparedStatement.setLong(4, user.getSesso());
+            preparedStatement.setLong(3, user.getEta()); // TODO: fare Tostring
+            preparedStatement.setLong(4, user.getSesso()); // TODO: fare Tostring
             preparedStatement.setString(5, user.getEmail());
             preparedStatement.setString(6, user.getNumero());
             preparedStatement.setString(7, user.getPassword());
@@ -89,17 +89,32 @@ public class databaseDAO {
 	public void insertInstru(istruttore istr) throws SQLException {
         System.out.println(INSERT_INSTRU_SQL);
         // try-with-resource statement will auto close the connection.
-        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INSTRU_SQL)) {
             preparedStatement.setString(1, istr.getNome());
             preparedStatement.setString(2, istr.getCognome());
-            preparedStatement.setLong(3, istr.getEta());
-            preparedStatement.setLong(4, istr.getSesso());
+            preparedStatement.setLong(3, istr.getEta()); // TODO: fare Tostring
+            preparedStatement.setLong(4, istr.getSesso()); // TODO: fare Tostring
             preparedStatement.setString(5, istr.getEmail());
             preparedStatement.setString(6, istr.getNumero());
             preparedStatement.setString(7, istr.getPassword());
-            preparedStatement.setLong(8, istr.getEsperienza());
-            preparedStatement.setLong(9, istr.getOreLezione());
+            preparedStatement.setLong(8, istr.getEsperienza()); // TODO: fare Tostring
+            preparedStatement.setLong(9, istr.getOreLezione()); // TODO: fare Tostring
             preparedStatement.setString(10, Float.toString(istr.getPagaOraria()));
+            System.out.println(preparedStatement);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("SQLState: " +((SQLException)e).getSQLState());
+        }
+    }
+	
+	public void insertField(campo c) throws SQLException {
+        System.out.println(INSERT_FIELDS_SQL);
+        // try-with-resource statement will auto close the connection.
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_FIELDS_SQL)) {
+            preparedStatement.setString(1, c.getTipo());
+            preparedStatement.setString(2, Float.toString(c.getPrezzo()));
+            preparedStatement.setString(3, Integer.toString(c.getValutazione()));
+            preparedStatement.setString(4, Boolean.toString(c.isCoperto()));
             System.out.println(preparedStatement);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
