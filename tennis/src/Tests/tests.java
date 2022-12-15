e02994823afd35e7ad2c75f8bd9dbced91ef4070
AlbertoGiovannoni <alberto.giovannoni@stud.unifi.it>
2022-12-15 12:15:33 +0100

    Junit

---
+++ tennis/src/Tests/tests.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class tests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void lambdaExpressions() {
+	    List numbers = Arrays.asList(1, 2, 3);
+	    assertTrue(5>0);
+	}
+
+}
--- tennis/src/module-info.java
+++ tennis/src/module-info.java
@@ -8,4 +8,5 @@
 module tennis {
 	requires java.sql;
 	requires javax.mail.api;
-}
+	requires org.junit.jupiter.api;
+	requires junit;}
